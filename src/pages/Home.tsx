ea58378afdb72ddadfdae6652f72b13c66556f21
JustAPie <justapie06@gmail.com>
2023-10-10 21:12:18 +0700

    grammar error

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -90,7 +90,7 @@ export default function Home(): JSX.Element {
                                     <h2 className="text-xl text-white font-bold">Language</h2>
                                     <SmallContainer className="rounded-lg p-3">
                                         <h1 className="font-bold text-white">IELTS 7.5 (2023 - 2025)</h1>
-                                        <h1 className="font-bold text-white">Started learning Japanese from 2023</h1>
+                                        <h1 className="font-bold text-white">Started learning Japanese since 2023</h1>
 
                                     </SmallContainer>
                                     <h2 className="text-xl text-white font-bold">Computer Science</h2>
