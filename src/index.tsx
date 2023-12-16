65795d0ef5e77ec89f887338556629c3ff668130
JustAPie <justapie06@gmail.com>
2023-12-16 16:18:24 +0700

    initialize new project

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Routes } from "@generouted/react-router";
+import { ChakraProvider } from "@chakra-ui/react";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ChakraProvider>
+      <Routes />
+    </ChakraProvider>
+  </React.StrictMode>
+);
