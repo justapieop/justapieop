190f366e7d329715d7732588d796d02652873762
Do Tran Thai An <justapie06@gmail.com>
2024-05-03 17:22:12 +0700

    testing r2 objects

--- src/app/404/page.tsx
+++ src/app/404/page.tsx
@@ -1,10 +1,9 @@
 import { Center, Image } from "@chakra-ui/react";
-import NotFoundImage from "../../assets/404.webp";
 
 export default function NotFound(): JSX.Element {
     return (
         <Center display="flex" backgroundColor="#000001" justifyContent="center" height="100vh">
-            <Image dropShadow="md" margin="auto" objectFit="scale-down" maxHeight="100%" src={NotFoundImage.src} />
+            <Image dropShadow="md" margin="auto" objectFit="scale-down" maxHeight="100%" src="https://s3.justapie.net/404.webp" />
         </Center>
     );
 }
