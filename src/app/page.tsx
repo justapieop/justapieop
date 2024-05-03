eedbead86c6b961c23a1a20a0011366e380c9128
Do Tran Thai An <justapie06@gmail.com>
2024-05-03 13:13:38 +0700

    compress media

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import { Container, Card, CardHeader, Text, Avatar, Box, Flex, Button, CardFooter, Link, Image } from "@chakra-ui/react";
 import { FaDiscord, FaGithub, FaTwitter } from "react-icons/fa";
-import AvatarImg from "../assets/avatar.png";
+import AvatarImg from "../assets/avatar.webp";
 
 export default function Home(): JSX.Element {
   return (
     <Box width="100%" height="100vh">
       <Box position="absolute" top="0" left="0" width="100%" height="100%" backgroundColor="#000000ac" />
-      <video src="/yae.mp4" height="100vh" width="100%" autoPlay loop muted preload="auto" />
+      <video src="/yae.webm" height="100vh" width="100%" autoPlay loop muted preload="auto" />
       <Box position="absolute" top="0" width="100%" height="100%" display="flex" flexDirection="column" justifyContent="center" alignItems="flex-start">
         <Container>
           <Card maxWidth="md" rounded="2xl" right={{ xl: "150px" }} opacity="0.85" backgroundColor="#2d2b2baa">
