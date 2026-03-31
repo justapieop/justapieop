d57d4ee54880fd14a422b9ed5ce781421bcc5c14
Do Tran Thai An <justapie06@gmail.com>
2026-03-31 23:56:15 +0700

    chore: integrate generouted for routing and update TypeScript configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Routes } from "@generouted/react-router";
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <Routes />
+  </StrictMode>,
+);
